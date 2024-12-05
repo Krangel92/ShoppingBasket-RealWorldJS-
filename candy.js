32d5686a71b8e548337437805e250b28ac6e2f74
Karla Rangel <krangelhdz@gmail.com>
2024-12-05 12:12:37 +0000

    wrote the Candy class

---
+++ candy.js
@@ -0,0 +1,14 @@
+class Candy {
+	constructor(candyName, price) {
+		this.candyName = candyName;
+		this.price = price;
+	}
+
+	getName() {
+		return this.candyName;
+	}
+
+	getPrice() {
+		return this.price;
+	}
+}
