87a80cb8de436f9c2f4e8d9eeb439de9164c478e
Karla Rangel <krangelhdz@gmail.com>
2024-12-09 15:12:51 +0000

    added note

--- candy.test.js
+++ candy.test.js
@@ -1,4 +1,7 @@
+//
+
 const Candy = require("./candy.js");
+// let candy1 = new Candy("Twix", 3.5);  -> if I create the instance here, instead of lines 9 & 15, both 'it' can access iit.
 
 describe("candy", () => {
 	it("takes a candy name & its price and the getName method returns the name", () => {
@@ -9,8 +12,8 @@ describe("candy", () => {
 		expect(candy1.getName()).toBe("Twix");
 	});
 	it("takes a candy name & its price and the getPrice method returns the price", () => {
-		let candy2 = new Candy("Twix", 3.5);
+		let candy1 = new Candy("Twix", 3.5);
 
-		expect(candy2.getPrice()).toBe(3.5);
+		expect(candy1.getPrice()).toBe(3.5);
 	});
 });
