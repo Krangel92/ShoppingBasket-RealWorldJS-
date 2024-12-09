1a7f35d5653a2e6d195bb579f5fb3fc6c3fe68f2
Karla Rangel <krangelhdz@gmail.com>
2024-12-09 14:50:03 +0000

    created test 2 for the Candy class getPrice()

--- candy.test.js
+++ candy.test.js
@@ -1,11 +1,16 @@
 const Candy = require("./candy.js");
 
 describe("candy", () => {
-	it("takes a candy name & its price and returns the name", () => {
+	it("takes a candy name & its price and the getName method returns the name", () => {
 		// Create an instance of the Candy class:
 		let candy1 = new Candy("Twix", 3.5);
 
 		// Test the getName method on the instance created:
 		expect(candy1.getName()).toBe("Twix");
+	});
+	it("takes a candy name & its price and the getPrice method returns the price", () => {
+		let candy2 = new Candy("Twix", 3.5);
+
+		expect(candy2.getPrice()).toBe(3.5);
 	});
 });
