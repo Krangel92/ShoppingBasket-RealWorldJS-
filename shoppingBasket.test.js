55cf88ed5d09a7dd8f633bb8cd5514df611d60d9
Karla Rangel <krangelhdz@gmail.com>
2024-12-10 16:00:48 +0000

    wrote test for the ShoppingBasket class

---
+++ shoppingBasket.test.js
@@ -0,0 +1,20 @@
+// Exercise (instructions):
+// write tests for the Shoppingbasket class using a JS object as a "double" or mock.
+
+const ShoppingBasket = require("./shoppingBasket.js");
+
+describe("shoppingBasket", () => {
+	it("adds the candy price to the initially empty basket", () => {
+		// Create a fake candy object:
+		let fakeCandy = { getName: () => "Twix", getPrice: () => 3.5 };
+
+		// Create an instance of ShoppingBasket:
+		let basket = new ShoppingBasket();
+
+		// Add the fake candy to the basket:
+		basket.addItem(fakeCandy);
+
+		// Verify the total price:
+		expect(basket.getTotalPrice()).toBe(3.5);
+	});
+});
